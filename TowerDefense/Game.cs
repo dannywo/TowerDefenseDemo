9f79bb8d939aead5331c4ebe7c8b5f362a03a8fc
dorellan <dorellan@APPDEV-XPS>
2017-03-15 18:07:39 -0700

    Add project files.

---
+++ TowerDefense/Game.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TowerDefense
+{
+    class Game
+    {
+        static void Main()
+        {
+            Tower tower = new Tower();
+            Map map = new Map(8, 5);
+
+            int area = map.Width * map.Height;
+
+            Point point = new Point(4, 2);
+            bool isOnMap = map.OnMap(point);
+            Console.WriteLine(isOnMap); // return true
+
+            point = new Point(8, 5);
+            isOnMap = map.OnMap(point); // returns false
+            Console.WriteLine(isOnMap);
+            Console.ReadKey();
+        }
+    }
+}
